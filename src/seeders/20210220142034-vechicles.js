b7faa1ce88407d113062ad1361478d5f0197cff4
Joyston <joyco107@gmail.com>
2021-02-20 22:29:02 +0530

    Added migration auto run

--- src/seeders/20210220142034-vechicles.js
+++ src/seeders/20210220142034-vechicles.js
@@ -11,6 +11,10 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+
+   await queryInterface.bulkInsert('Vechicles', [
+      {latitude: 18.6035983, longitude:73.7576216, time:'2018-09-09 09:00:26+05:30', license:'ADTEST1', model: 'MITSUBISHI', engine:'1DZ0037452', chasis: '839F23-23616'}
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
